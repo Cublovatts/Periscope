46cc1454cd5ca0c1a5df9843886a227d25aa9e6e
Cublovatts <george.a.lovatt@gmail.com>
2023-04-11 21:08:02 +0100

    Added speed factor for guitar hero

--- SliderScript.cs
+++ SliderScript.cs
@@ -5,17 +5,18 @@ public class SliderScript : MonoBehaviour
     [SerializeField]
     private float speed;
     private bool isPlaying = false;
+    private float resolutionSpeedFactor;
 
     void Start()
     {
-        
+        resolutionSpeedFactor = Screen.height / 1080f;
     }
 
     void Update()
     {
         if (isPlaying)
         {
-            transform.position = transform.position + (Vector3.down * (speed * Time.deltaTime));
+            transform.position = transform.position + (Vector3.down * (speed * resolutionSpeedFactor * Time.deltaTime));
         }
     }
 
